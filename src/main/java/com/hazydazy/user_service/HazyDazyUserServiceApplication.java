80c17c192cebba8ab162f6b1a97501fcdb8efac0
diogoMSF99 <diogo.msf.99@gmail.com>
2026-01-11 17:47:31 +0000

    chore: bootstrap SpringBoot backend service (user-service)

---
+++ src/main/java/com/hazydazy/user_service/HazyDazyUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hazydazy.user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HazyDazyUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HazyDazyUserServiceApplication.class, args);
+	}
+
+}
