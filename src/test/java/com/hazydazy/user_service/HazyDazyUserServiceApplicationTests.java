243534a81efca67a8b0ad5abc8fe7eeb482ddb58
Diogo Ferreira <diogo.msf.99@gmail.com>
2026-01-29 16:30:47 +0000

    fix spring test runner

--- src/test/java/com/hazydazy/user_service/HazyDazyUserServiceApplicationTests.java
+++ src/test/java/com/hazydazy/user_service/HazyDazyUserServiceApplicationTests.java
@@ -2,12 +2,13 @@ package com.hazydazy.user_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class HazyDazyUserServiceApplicationTests {
 
-	@SpringBootTest
-	@ActiveProfiles("test")
+	@Test
 	void contextLoads() {
 	}
 
